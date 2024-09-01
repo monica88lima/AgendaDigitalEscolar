2b743cbb97038062ed53ee3fbd4c988004758d1b
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-09-01 17:51:20 -0300

    Removendo lixo e adicionando router-dom e ant-design

--- agenda-escolar/src/App.js
+++ agenda-escolar/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
