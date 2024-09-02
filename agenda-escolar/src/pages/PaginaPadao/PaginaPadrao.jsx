e35cbfc9d8305d37fe86f5e85d715aaaf841b670
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-09-02 13:18:02 -0300

    srAdicionando PaginaPadrão

---
+++ agenda-escolar/src/pages/PaginaPadao/PaginaPadrao.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import { Row } from 'antd'
+
+const PaginaPadrao = () => {
+  return (
+    <div>
+        {/* <Row> */}
+            <Header/>
+        {/* </Row> */}
+    </div>
+  )
+}
+
+export default PaginaPadrao
